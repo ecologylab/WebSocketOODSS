aeb29247d12b36cdb82cc835a365affe17b62127
rhema <rhema@ecologylab.net>
2011-08-23 01:15:52 +0000

    Refactoring.

--- src/ecologylab/oodss/server/clientsessionmanager/NewDatagramClientSessionManager.java
+++ src/ecologylab/oodss/server/clientsessionmanager/NewDatagramClientSessionManager.java
@@ -8,7 +8,7 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 
 import ecologylab.collections.Scope;
-import ecologylab.oodss.distributed.server.AIODatagramServer;
+//import ecologylab.oodss.distributed.server.AIODatagramServer;
 import ecologylab.oodss.messages.RequestMessage;
 import ecologylab.oodss.messages.ResponseMessage;
 import ecologylab.oodss.messages.UpdateMessage;
@@ -21,7 +21,7 @@ public class NewDatagramClientSessionManager extends NewBaseSessionManager
 {
 	InetSocketAddress	address;
 	
-	AIODatagramServer server;
+	//AIODatagramServer server;
 	
 	SelectionKey mostRecentKey;
 	
@@ -30,13 +30,13 @@ public class NewDatagramClientSessionManager extends NewBaseSessionManager
 	 * @param socket
 	 * @param baseScope
 	 */
-	public NewDatagramClientSessionManager(String sessionId, AIODatagramServer aioDatagramServer,
+	public NewDatagramClientSessionManager(String sessionId, /*AIODatagramServer aioDatagramServer,*/
 			SelectionKey socket, Scope<?> baseScope, InetSocketAddress	address)
 	{
 		super(sessionId, socket, baseScope);
 		
 		this.address = address;
-		this.server = aioDatagramServer;
+	//	this.server = aioDatagramServer;
 	}
 
 	/**
@@ -59,7 +59,8 @@ public class NewDatagramClientSessionManager extends NewBaseSessionManager
 	@Override
   public void sendUpdateToClient(UpdateMessage update) 
 	{
-		server.sendMessage(update, mostRecentKey, (long) -1, getAddress());
+		//server.sendMessage(update, mostRecentKey, (long) -1, getAddress());
+		System.out.println("server.sendMessage SHould have been called but was not");
   }
 
 	public void updateKey(SelectionKey key) 
